7b5753e9b79f2c2f960b23dae62234d039584c3d
sachincoolkarni <sachin86.kulkarni@gmail.com>
2022-12-01 10:57:09 +0530

    Flowable POC

---
+++ src/main/java/org/example/CallExternalSystemDelegate.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+public class CallExternalSystemDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        System.out.println("calling external System"+delegateExecution.getVariable("employee"));
+    }
+}
---
+++ src/main/java/org/example/SendRejectionMail.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+public class SendRejectionMail implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        System.out.println("sending rejection mail");
+    }
+}
